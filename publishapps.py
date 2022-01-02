04149dfeef52bc8ba13d4df0dcfae90527edc213
the-boss-omega <88785685+the-boss-omega@users.noreply.github.com>
2022-01-02 14:59:31 +0200

    Update publishapps.py

--- publishapps.py
+++ publishapps.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.title('the king of the dice')
 st.subheader('introdocing the first special digita lotter')
 st.write('''click here to try the win
-enjoy it but npt to much''')
+enjoy it but npt to much
+''')
