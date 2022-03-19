8d183050d553da575cdd50c1606cfd23c1a0bb4b
the-boss-omega <88785685+the-boss-omega@users.noreply.github.com>
2022-03-19 11:51:22 +0200

    Update publishapps.py

--- publishapps.py
+++ publishapps.py
@@ -1,7 +1,16 @@
 import streamlit as st
-
+import pandas
+data = {
+  'x': [1,3,5,7,9,11,13],
+  'y': [200,400,600,800,1000,1200,1400]
+}
+df = pandas.DataFrame(data)
 st.title('the king of the dice')
 st.subheader('introdocing the first special digita lotter')
 st.write('''click here to try the win
-enjoy it but npt to much
-''')
+enjoy it but npt to much''')
+st.write(df)
+st.line_chart(df)
+st.area_chart(df)
+myslider = st.slider('Celsius')
+st.write(myslider,'in farenheit',myslider * 9/5 + 32)
